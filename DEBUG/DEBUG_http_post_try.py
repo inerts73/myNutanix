97669d98117e2519a09495722a444536afa0d4d0
Adam Hsu <adam.hsu@nutanix.com>
2019-03-27 17:33:06 -0700

    AZ

--- DEBUG/DEBUG_http_post_try.py
+++ DEBUG/DEBUG_http_post_try.py
@@ -1,6 +1,15 @@
 import json
 from requests import Session
 
+def output_to_json_file(URL, METHOD, DATA):
+  res = send_req(url=URL, method=METHOD, data=DATA)
+  print
+  print 'content => ', json.dumps(json.loads(res.content), indent=2)
+  print 'status => ', res
+  with open("TEMP_http_post_try.josn", "w") as fp:
+    json.dump(json.loads(res.content), fp, indent=2)
+  print "content saved in => TEMP_http_post_try.json"
+
 def send_req(url, method, params={}, data={},
                  header={'content-type': 'application/json;charset=UTF-8',
                          'Accept': 'application/json,'
@@ -10,6 +19,35 @@ def send_req(url, method, params={}, data={},
     return Session().request(url=url, method=method, params=params,
                              data=data, headers=header, auth=auth,
                              timeout=timeout, verify=verify)
+
+##################################################
+
+def az_list():
+  URL = 'https://10.40.216.115:9440/api/nutanix/v3/availability_zones/list'
+  METHOD = 'post'
+  DATA = {"kind": "availability_zone", "length": 500, "offset": 0}
+  DATA = json.dumps(DATA)
+
+  return URL, METHOD, DATA
+
+def replicate_from_srouce_to_remote():
+  URL = 'https://10.40.216.115:9440/api/nutanix/v3/recovery_points/01cc10a9-d6ec-42e4-826b-7d4f6fca79fa/replicate'
+  METHOD = 'post'
+  DATA = {
+    "source_availability_zone_reference": {
+      "kind": "availability_zone",
+      "name": "Local AZ",
+      "uuid": "c82446a8-8d05-4530-8b2f-075b48a5658c"
+    },
+    "target_availability_zone_reference": {
+      "kind": "availability_zone",
+      "name": "PC_10.40.184.38",
+      "uuid": "f69161f0-3f4f-4c7a-a9f5-156b20aa8453"
+    }
+  }
+  DATA = json.dumps(DATA)
+
+  return URL, METHOD, DATA
 
 def restore_on_pc():
   URL = 'https://10.40.216.115:9440/api/nutanix/v3/recovery_points/6bc5d236-2aa2-498c-8563-31f43f799285/restore'
@@ -58,7 +96,7 @@ def restore_on_pc():
 
   return URL, METHOD, DATA
 
-def get_vm_info():
+def vm_info():
     URL = 'https://10.40.216.115:9440/api/nutanix/v3/vms/6e180a9d-3bfe-5d75-af25-1263d879eb7a'
     METHOD = 'get'
     DATA = {}
@@ -66,17 +104,10 @@ def get_vm_info():
 
     return URL, METHOD, DATA
 
-def output_to_json_file(URL, METHOD, DATA):
-  res = send_req(url=URL, method=METHOD, data=DATA)
-  print
-  print 'content => ', json.dumps(json.loads(res.content), indent=2)
-  print 'status => ', res
-  with open("TEMP_http_post_try.josn", "w") as fp:
-    json.dump(json.loads(res.content), fp, indent=2)
-  print "content saved in => TEMP_http_post_try.json"
-
 if __name__ == '__main__':
-    output_to_json_file(*restore_on_pc())
-    # output_to_json_file(*get_vm_info())
+  output_to_json_file(*az_list())
+  #output_to_json_file(*replicate_from_srouce_to_remote())
+  #output_to_json_file(*restore_on_pc())
+  #output_to_json_file(*vm_info())
 
 
